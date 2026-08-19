a5d6da816ce5ca1ac36396c856a608f0c93a1586
NIIIIIIIIIIIIIIIIIIIIIIIk <nikitvs2005@gmail.com>
2026-08-19 14:14:36 +0300

    Delete src directory

--- src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DIRECTOR_NAME = 'NIK';
-export const MENEGER_NAME = 'LISIK';
-export const PRODUCER_NAME = 'PRODUCER';
-
-export const FOOTER_TEXT = 
-  'Все встречи носят рекомендательный характер. Утверждено на художественном совете. Правки принимаются в мягкой форме.';
