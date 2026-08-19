fbca3136451c89bcb90611e804926145324519cf
NIIIIIIIIIIIIIIIIIIIIIIIk <nikitvs2005@gmail.com>
2026-08-19 13:25:40 +0300

    Add files via upload

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const DIRECTOR_NAME = 'NIK';
+export const MENEGER_NAME = 'LISIK';
+
+export const FOOTER_TEXT = 
+  'Все встречи носят рекомендательный характер. Утверждено на художественном совете. Правки принимаются в мягкой форме.';
